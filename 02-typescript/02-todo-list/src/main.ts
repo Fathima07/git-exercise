9418358273f7b553f689a72f4e503ee82a7a2ede
Fathima07 <fathima.abdul.azeez@powercoders.org>
2021-06-09 21:24:40 +0200

    todo list

--- 02-typescript/02-todo-list/src/main.ts
+++ 02-typescript/02-todo-list/src/main.ts
@@ -1,8 +1,11 @@
 const getDiv = document.getElementById("to-do");
-
+const crtUl = document.createElement("ul");
+const crtInput = document.createElement("input");
+const crtFilter = <HTMLInputElement>document.getElementById("filter-todo");
 if(!(getDiv instanceof HTMLDivElement)) {
   throw new Error("No div with id 'to-do' found!");
 }
+
 
 type State = {
   title: string;
@@ -28,33 +31,62 @@ const toDos: State = [
   },
 ];
 
+
 const crtToDo = () => {
   getDiv.innerHTML= "";
-  let crtInput = document.createElement("input");
+ 
   crtInput.setAttribute("type","text");
   crtInput.innerHTML= " ";
   getDiv.appendChild(crtInput);
-  let crtUl = document.createElement("ul");
-  getDiv.appendChild(crtUl)
+  
+  getDiv.appendChild(crtUl);
+  
+  let crtButton = document.createElement('button');
+  listTodo();
+  crtButton.innerText ="Add";
+  getDiv.appendChild(crtButton)
+  crtButton.addEventListener("click",addTodo);
+  crtFilter?.addEventListener("click",function(){
+    listTodo(crtFilter?.checked);
+  });
+}
 
-  for(const todo of toDos) {
+
+const addTodo =()=>{
+  toDos.push({
+    'title':crtInput.value,
+    'done':false
+  });
+  listTodo();
+}
+
+const listTodo=(filter?:boolean)=>{
+  crtUl.innerHTML = '';
+  for(const todo of toDos.sort((a, b) => a.title.localeCompare(b.title))) {
+    if((filter && !todo.done)){
+      continue;
+    }
     let crtLi = document.createElement('li');
     let checkInput = document.createElement('input');
     checkInput.setAttribute("type","checkbox");
     checkInput.checked = todo.done;
     crtLi.appendChild(checkInput);
+    checkInput.addEventListener("click",function(){
+      if(checkInput.value){
+        todo.done = checkInput.checked;
+        console.log(todo);
+      }
+    });
+    let crtSpan = document.createElement("span");
+    crtSpan.innerHTML = "Title:" + todo.title;
+    crtLi.appendChild(crtSpan);
     crtUl.appendChild(crtLi);
-     crtLi.textContent = "Title:" + todo.title;
+
+    // crtLi.textContent = "Title:" + todo.title;
      
   }
-  
-  let crtButton = document.createElement('button');
-  crtButton.innerText ="Add";
-  getDiv.appendChild(crtButton)
 }
-
 crtToDo();
-
 /*
 const addToDoList = () => {
    let int = document.querySelector('crtInput')?.value;
