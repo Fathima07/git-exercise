1162299394a3eb90d017cfbfb46a6d6cda9678e4
Fathima07 <fathima.abdul.azeez@powercoders.org>
2021-06-10 20:41:45 +0200

    User list function

---
+++ 03-react/02-users-list/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.tsx',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      { // to support css I added css-loader
+        test: /\.css$/,
+        use: ['css-loader']
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
