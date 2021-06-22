3709cad23adfda509d2dd8916b7e2810ad62b15f
Fathima07 <81806861+Fathima07@users.noreply.github.com>
2021-06-22 10:01:04 +0200

    Feature participants (#20)
    * participants github
    
    * corrected the problems

---
+++ 03-react/08-powercoders-participants/src/components/Home.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import {
+	Link,
+} from "react-router-dom";
+import { useParams } from "react-router-dom";
+
+
+
+interface Participants {
+	login: string;
+	id: number;
+	avatar_url: string;
+
+}
+
+
+export default function Home() {
+	const [users, setUsers] =
+		React.useState<Participants[]>();
+
+
+
+	React.useEffect(() => {
+		fetch(
+			`https://api.github.com/repos/Powercoders-Switzerland/2021-1-web-dev/contributors`
+		)
+			.then((response) => response.json())
+			.then(data => {
+				console.log(data)
+				setUsers(data)
+
+			})
+	}, []);
+
+
+	console.log(users);
+	return (
+		<main>
+			{
+				users ? users.map((use): JSX.Element => {
+					return <article key={use.id} className="home">
+						<p ></p>
+						<p className="link"><Link  to={'/users/' + use.login}><h3>{use.login}</h3><img src={use.avatar_url} /></Link>
+						</p>
+					
+						
+						
+					</article>
+				}) : 'there is no Participants'
+			}
+		</main>
+	);
+}
